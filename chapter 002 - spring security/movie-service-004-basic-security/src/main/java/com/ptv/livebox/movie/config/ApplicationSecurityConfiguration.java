09857f584a15bf45810527afef05a43808eb3717
Mazhar Hassan <netsurgon@gmail.com>
2020-10-15 12:02:49 +0500

    Basic authentication with custom user name and password

--- chapter 002 - spring security/movie-service-004-basic-security/src/main/java/com/ptv/livebox/movie/config/ApplicationSecurityConfiguration.java
+++ chapter 002 - spring security/movie-service-004-basic-security/src/main/java/com/ptv/livebox/movie/config/ApplicationSecurityConfiguration.java
@@ -1,9 +1,17 @@
 package com.ptv.livebox.movie.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -18,5 +26,23 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        List<UserDetails> users = new ArrayList<>();
+        users.add(User.withDefaultPasswordEncoder()
+                .username("xman")
+                .password("firstclass")
+                .roles("USER").build());
+
+        users.add(User.withDefaultPasswordEncoder()
+                .username("charles")
+                .password("firstclass")
+                .roles("USER", "ADMIN").build());
+
+        return new InMemoryUserDetailsManager(users);
+        //return super.userDetailsServiceBean();
     }
 }

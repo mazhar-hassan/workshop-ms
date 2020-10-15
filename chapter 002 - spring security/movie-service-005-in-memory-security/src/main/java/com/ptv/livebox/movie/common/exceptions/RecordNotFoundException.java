bd266352e4dd64f4f3c31114690cd462a6bef168
Mazhar Hassan <netsurgon@gmail.com>
2020-10-15 09:39:09 +0500

    -- clone project --

---
+++ chapter 002 - spring security/movie-service-005-in-memory-security/src/main/java/com/ptv/livebox/movie/common/exceptions/RecordNotFoundException.java
@@ -0,0 +1,11 @@
+package com.ptv.livebox.movie.common.exceptions;
+
+public class RecordNotFoundException extends ApplicationException {
+    public RecordNotFoundException(String message) {
+        this(message, null);
+    }
+
+    public RecordNotFoundException(String message, Throwable cause) {
+        super(ERROR_RECORD_NOT_FOUND, message, cause);
+    }
+}
---
+++ chapter 002 - spring security/movie-service-005-in-memory-security/src/main/java/com/ptv/livebox/movie/dao/MovieRepository.java
@@ -0,0 +1,10 @@
+package com.ptv.livebox.movie.dao;
+
+import com.ptv.livebox.movie.dao.entity.MovieEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<MovieEntity, Integer> {
+
+}

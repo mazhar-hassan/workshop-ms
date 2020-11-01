9b6815139aca99fcfed7ae09139467357564acd8
Mazhar Hassan <netsurgeon@gmail.com>
2020-11-01 22:10:15 +0500

    Add eureka server for service discovery
    update movies service to register itself with eureka server

---
+++ chapter 003 - auth service/ptv-discovery-server/src/test/java/com/ptv/livebox/discovery/WsDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptv.livebox.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- chapter 003 - auth service/ptv-movie-service/src/main/java/com/ptv/livebox/movie/common/GlobalExceptionHandler.java
+++ chapter 003 - auth service/ptv-movie-service/src/main/java/com/ptv/livebox/movie/common/GlobalExceptionHandler.java
@@ -6,7 +6,6 @@ import com.o4.microservices.common.exceptions.RecordNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -29,10 +28,10 @@ public class GlobalExceptionHandler {
             return handleApplicationException((ApplicationException) exception);
         } else if (exception instanceof HttpMessageNotReadableException) {
             return new ResponseEntity<>(ExceptionResponse.of(ApplicationException.ERROR_UNABLE_TO_MAP_OBJECT, "Parser error, please make sure JSON is properly formatted"), HttpStatus.INTERNAL_SERVER_ERROR);
-        } else if (exception instanceof AccessDeniedException) {
+        } /*else if (exception instanceof AccessDeniedException) {
             exception.printStackTrace();
             return new ResponseEntity<>(ExceptionResponse.of(ApplicationException.ERROR_ACCESS_FORBIDDEN, "Access is forbidden"), HttpStatus.FORBIDDEN);
-        }
+        }*/
 
         //log.error("System exception occurred", exception);
         exception.printStackTrace();
